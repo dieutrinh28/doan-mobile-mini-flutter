8e03b3dbf9a92b4794f652cbf432deffb03cb03a
dieutrinh28 <trinhdo.work@gmail.com>
2023-03-09 22:34:51 +0700

    login-page

---
+++ android/app/src/main/kotlin/com/example/doan_mini_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.doan_mini_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/login.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  bool isHiddenPassword = true;
+  bool isHidden = true;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  void onTogglePasswordVisible() {
+    setState(() {
+      isHiddenPassword = !isHiddenPassword;
+    });
+  }
+
+  void onLoginClick() {
+    final isValid = formKey.currentState?.validate();
+    if (isValid != null && isValid == true) {
+      formKey.currentState?.save();
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const HomePage()));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        shape: const Border(
+          bottom: BorderSide(
+            color: Color.fromRGBO(102, 102, 102, 0.08),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 0,
+          ),
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            color: Color(0xFF333232),
+            size: 24,
+          ),
+          onPressed: () {},
+        ),
+        title: const Text(
+          'Login',
+          style: TextStyle(
+            fontSize: 18,
+            color: Color(0xFF333232),
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 0,
+        ),
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 24),
+              const Text(
+                "Email",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                  color: Color(0xFF333232),
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: emailController,
+                cursorColor: const Color(0xFF228176),
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 10,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4),
+                    ),
+                    borderSide: BorderSide(
+                      width: 1,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFFE1E0E0),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF2A9D8F),
+                    ),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFFCA2E55),
+                    ),
+                  ),
+                  errorStyle: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    color: Color(0xFFCA2E55),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "this field is required!";
+                  } else if (!RegExp(r"^[a-zA-Z\d.]+@[a-zA-Z\d]+\.[a-zA-Z]+")
+                      .hasMatch(value)) {
+                    return "email invalid!";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                "Password",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14,
+                  color: Color(0xFF333232),
+                ),
+              ),
+              const SizedBox(height: 8),
+              TextFormField(
+                controller: passwordController,
+                cursorColor: const Color(0xFF228176),
+                obscureText: isHidden,
+                obscuringCharacter: '*',
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 10,
+                  ),
+                  suffixIcon: InkWell(
+                    onTap: () {
+                      setState(() {
+                        isHidden = !isHidden;
+                      });
+                    },
+                    child: SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: isHidden
+                          ? const Icon(Icons.visibility_off)
+                          : const Icon(Icons.visibility),
+                    ),
+                  ),
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4),
+                    ),
+                    borderSide: BorderSide(
+                      width: 1,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFFE1E0E0),
+                    ),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFF2A9D8F),
+                    ),
+                  ),
+                  errorBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color(0xFFCA2E55),
+                    ),
+                  ),
+                  errorStyle: const TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12,
+                    color: Color(0xFFCA2E55),
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "this field is required!";
+                  }
+                  if (value.length < 6) {
+                    return "password must from 6 chars!";
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 32),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onLoginClick,
+                  child: const Text('Login'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
